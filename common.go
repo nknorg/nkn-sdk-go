e1ed382ca1deda701b6eeb27542a59f81e2d7316
Nikolai Perevozchikov <trueinsider@gmail.com>
2019-01-28 06:21:09 +0300

    shuffle only if seed rpc addr wasn't set explicitly

--- common.go
+++ common.go
@@ -25,11 +25,13 @@ var SeedRPCServerAddr string
 var AssetId common.Uint256
 
 func Init() {
-	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(seedList), func(i int, j int) {
-		seedList[i], seedList[j] = seedList[j], seedList[i]
-	})
-	SeedRPCServerAddr = seedList[0]
+	if SeedRPCServerAddr == "" {
+		rand.Seed(time.Now().UnixNano())
+		rand.Shuffle(len(seedList), func(i int, j int) {
+			seedList[i], seedList[j] = seedList[j], seedList[i]
+		})
+		SeedRPCServerAddr = seedList[0]
+	}
 
 	tmp, _ := common.HexStringToBytesReverse("4945ca009174097e6614d306b66e1f9cb1fce586cb857729be9e1c5cc04c9c02")
 	if err := AssetId.Deserialize(bytes.NewReader(tmp)); err != nil {
